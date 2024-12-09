842fbd83c253cdb6610c789daa0366c274a475a6
K Laxman <71651991+Laxman824@users.noreply.github.com>
2024-12-10 00:22:47 +0530

    Update scraper.py

--- app/utils/scraper.py
+++ app/utils/scraper.py
@@ -1,7 +1,37 @@
+# # utils/scraper.py
+# import requests
+# import pandas as pd
+# from bs4 import BeautifulSoup
+# from config.config import Config
+
+# class IPOScraper:
+#     @staticmethod
+#     def scrape_ipo_data():
+#         try:
+#             response = requests.get(Config.IPO_URL)
+#             soup = BeautifulSoup(response.text, 'html.parser')
+            
+#             table = soup.find('table')
+#             df = pd.read_html(str(table))[0]
+            
+#             df.columns = ['Current IPOs', 'IPO GMP', 'IPO Price', 'Gain', 
+#                          'Kostak', 'Subject', 'Type']
+            
+#             # Clean data
+#             df['Gain'] = df['Gain'].str.rstrip('%').astype(float)
+#             df['Current IPOs'] = df['Current IPOs'].str.split('\n').str[0]
+#             df['Date'] = df['Current IPOs'].str.extract(
+#                 r'(\d{1,2}-\d{1,2}\s*(?:Dec|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov))')
+            
+#             return df
+#         except Exception as e:
+#             print(f"Error scraping data: {str(e)}")
+#             return None
 # utils/scraper.py
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from io import StringIO
 from config.config import Config
 
 class IPOScraper:
@@ -12,18 +42,26 @@ class IPOScraper:
             soup = BeautifulSoup(response.text, 'html.parser')
             
             table = soup.find('table')
-            df = pd.read_html(str(table))[0]
-            
-            df.columns = ['Current IPOs', 'IPO GMP', 'IPO Price', 'Gain', 
-                         'Kostak', 'Subject', 'Type']
-            
-            # Clean data
-            df['Gain'] = df['Gain'].str.rstrip('%').astype(float)
-            df['Current IPOs'] = df['Current IPOs'].str.split('\n').str[0]
-            df['Date'] = df['Current IPOs'].str.extract(
-                r'(\d{1,2}-\d{1,2}\s*(?:Dec|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov))')
-            
-            return df
+            if table:
+                # Convert table to string and wrap with StringIO
+                table_str = StringIO(str(table))
+                df = pd.read_html(table_str)[0]
+                
+                # Clean column names
+                df.columns = ['Current IPOs', 'IPO GMP', 'IPO Price', 'Gain', 
+                            'Kostak', 'Subject', 'Type']
+                
+                # Clean data
+                df['Gain'] = df['Gain'].str.rstrip('%').astype(float)
+                df['Current IPOs'] = df['Current IPOs'].str.split('\n').str[0]
+                df['Date'] = df['Current IPOs'].str.extract(
+                    r'(\d{1,2}-\d{1,2}\s*(?:Dec|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov))')
+                
+                return df
+            else:
+                print("No table found in the webpage")
+                return None
+                
         except Exception as e:
             print(f"Error scraping data: {str(e)}")
             return None
